c0260eeb90fb0d522280103815944c23715d92a1
Rohit Tripathi <rt264@snu.edu.in>
2024-07-09 01:38:37 +0530

    updated_front_page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from '@/components/Navbar';
+import AuthModal from '@/components/Modals/AuthModal';
 
 type AuthPageProps = {
     
@@ -6,6 +8,14 @@ type AuthPageProps = {
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     
-    return <div>Authentication Page</div>
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+        <Navbar />
+        <div className='flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
+					<img src='/hero.png' alt='Hero img' width={700} height={700} />
+				</div>
+                <AuthModal/>
+        </div>
+    </div>
 }
 export default AuthPage;
