349cc75bcde40df854f4c08bdb18f5929c443331
Rohit Tripathi <rt264@snu.edu.in>
2024-07-10 01:59:57 +0530

    before_firebase_done

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { authModalState } from "@/atoms/authModalAtom";
+import { useSetRecoilState } from "recoil";
 
 type SignupProps = {
     
 };
 
 const Signup:React.FC<SignupProps> = () => {
-    
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const handleClick = () => 
+        {
+		setAuthModalState((prev) => ({ ...prev, type: "login" }));
+	   };
     return (
 		<form className='space-y-6 px-6 pb-4' >
 			<h3 className='text-xl font-medium text-white'>Register to LeetClone</h3>
@@ -69,7 +75,7 @@ const Signup:React.FC<SignupProps> = () => {
 
 			<div className='text-sm font-medium text-gray-300'>
 				Already have an account?{" "}
-				<a href='#' className='text-blue-700 hover:underline' >
+				<a href='#' className='text-blue-700 hover:underline' onClick={handleClick}>
 					Log In
 				</a>
 			</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import { authModalState } from "@/atoms/authModalAtom";
+import { useSetRecoilState } from "recoil";
 
 type NavbarProps = {};
 
 const Navbar:React.FC<NavbarProps> = () =>
      {
-    
+        const setAuthModalState = useSetRecoilState(authModalState);
+        const handleClick = () => {
+            setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+        };
     return (
         <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
             <Link href='/' className='flex items-center justify-center h-20'>
@@ -14,7 +19,8 @@ const Navbar:React.FC<NavbarProps> = () =>
             <div className='flex items-center'>
 				<button className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
                 hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
-                transition duration-300 ease-in-out'>
+                transition duration-300 ease-in-out'
+                onClick={handleClick}>
                     Sign in
                 </button>
         </div>
