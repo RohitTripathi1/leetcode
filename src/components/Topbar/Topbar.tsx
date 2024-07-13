3ab0d659ae9a2b6b720970f291d87f1b32c928da
Rohit Tripathi <rt264@snu.edu.in>
2024-07-13 13:20:34 +0530

    1st_part_done

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import Logout from "../Buttons/Logout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => 
     {
+        const [user] = useAuthState(auth);
+        const setAuthModalState=useSetRecoilState(authModalState);
 	return (
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={"flex w-full items-center justify-between max-w-[1200px] mx-auto" }>
@@ -26,11 +33,27 @@ const Topbar: React.FC<TopbarProps> = () =>
 							Premium
 						</a>
 					</div>
-					
-						<Link href='/auth'>
+					{!user &&(<Link href='/auth' onClick={()=>{
+                        setAuthModalState((prev)=>({...prev,isOpen:true,type:"login"}));
+
+                    }}>
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
-						</Link>
-					
+						</Link>)}
+						{user &&
+                        (
+                            <div className='cursor-pointer group relative'>
+							<img src='/avatar.png' alt='user profile image'  className='rounded-full h-8 w-8' />
+							 <div
+								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out'
+							>
+								<p className='text-sm'>{user.email}</p>
+							</div> 
+						</div>
+                        )}
+                        {/* if user is logged in them only show logout button */}
+					{user &&<Logout/>}
 				</div>
 			</div>
 		</nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
@@ -14,7 +17,8 @@ export default function App({ Component, pageProps }: AppProps) {
 					content='Web application that contains leetcode problems and video solutions'
 				/>
 			</Head>
-
+     
+     <ToastContainer/>
     <Component {...pageProps}/>
     </RecoilRoot>
   )
